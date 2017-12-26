50c467b70e6ad0102e121adcdaa14fe658530ffa
atswe <31803973+atswe@users.noreply.github.com>
2017-12-26 12:05:38 +0530

    Add files via upload

---
+++ global.php
@@ -0,0 +1,16 @@
+<?php
+$x = 5;
+$y = 10;
+
+function myTest() {
+    global $x, $y;
+    $y = $x + $y;
+}
+
+myTest();
+echo $y; // outputs 15
+
+
+//PHP also stores all global variables in an array called $GLOBALS[index]. 
+?>
+
